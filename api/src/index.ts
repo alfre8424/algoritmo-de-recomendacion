86940fb765442bf704d59fcc55ac570d942dea80
Joel Garcia <joel@ecx-labs.com>
2023-01-15 18:14:05 -0500

    Products query on backend

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,17 +2,20 @@ import express from 'express';
 import AuthAPI from './presentation/api/auth';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import ProductController from './presentation/api/products';
 
 const app = express();
 app.use(cors());
 
 // APIS
 const authAPI = new AuthAPI();
+const productAPI = new ProductController();
 
 // MIDDLEWARES
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(authAPI.router);
+app.use(productAPI.router);
 
 app.listen(process.env.API_PORT, () => {
 	console.log(`Server started on port ${process.env.API_PORT}`);
