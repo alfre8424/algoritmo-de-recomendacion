e502891128cda3286aef49035094ad00f77a3cd3
Joel Garcia <joel@ecx-labs.com>
2023-02-04 17:17:40 -0500

    modelo predict

--- client/src/presentation/views/home/components/component.search.tsx
+++ client/src/presentation/views/home/components/component.search.tsx
@@ -1,12 +1,16 @@
-import {SearchOutlined} from "@mui/icons-material";
+import { SearchOutlined } from "@mui/icons-material";
 import Button from "@mui/material/Button";
 import InputLabel from "@mui/material/InputLabel";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import AppSimpleDialog from "core/shared_components/component.dialog";
 import ListView from "core/shared_components/component.listview";
+import { ProductCard } from "core/shared_components/component.product_card";
 import SearchAppBar from "core/shared_components/component.searchbar";
 import ProductsController from "presentation/logic/products/controller";
-import {AppDispatch, RootState} from "presentation/logic/redux_config";
-import {ReactElement, useState} from "react"
-import {useDispatch, useSelector} from "react-redux";
+import { AppDispatch, RootState } from "presentation/logic/redux_config";
+import { ReactElement, useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
 
 interface IAppSearchBarProps {
 	onSearch: () => void;
@@ -23,8 +27,10 @@ export function AppSearch({
 	});
 
 	const [search, setSearch] = useState<string>("");
+	const [showAlert, setShowAlert] = useState<boolean>(false);
+	const [surveyRatings, setSurveyRatings] = useState<number[]>([0, 0, 0]);
 
-	const {products, searchedProducts} = useSelector((state: RootState) => state.products);
+	const { products, searchedProducts } = useSelector((state: RootState) => state.products);
 	return (
 		<div
 			className="bg-white flex px-8 shadow-md rounded-md py-8 flex-col h-full w-full items-center justify-start"
@@ -60,9 +66,59 @@ export function AppSearch({
 				<InputLabel className="w-full text-center md:text-left">
 					Mostrando productos disponibles ({products?.length})
 				</InputLabel>
-				<Button variant="outlined">Predecir</Button>
+				<Button
+					variant="outlined"
+					onClick={() => {
+						setShowAlert(true);
+					}}
+				>Predecir</Button>
 			</div>
 			<ListView />
+			<AppSimpleDialog
+				isOpen={showAlert}
+				title="Resultado de la búsqueda"
+				content={
+					<div>
+						<h1>Prediccion de canasta</h1>
+						<br />
+						<h2
+							className="text-lg font-semibold"
+						>
+							Encuesta de calidad
+						</h2>
+						<span>
+							Por favor, indiquenos la calidad de los siguientes apartados en el local 'local'
+						</span>
+						<br /><br />
+						<Typography component="legend">¿Qué tan buena fue la atención brindada?</Typography>
+						<Rating
+							name="survey1"
+							value={surveyRatings[0]}
+							onChange={(_, newValue) => {
+								setSurveyRatings([newValue ?? 0, surveyRatings[1], surveyRatings[2]]);
+							}}
+						/>
+						<br />
+						<Typography component="legend">¿Qué tanta variedad de productos hay en el local?</Typography>
+						<Rating
+							name="suervey2"
+							value={surveyRatings[1]}
+							onChange={(_, newValue) => {
+								setSurveyRatings([surveyRatings[0], newValue ?? 0, surveyRatings[2]]);
+							}}
+						/>
+						<Typography component="legend">¿Qué tan probable es que recomiende el local?</Typography>
+						<Rating
+							name="suervey2"
+							value={surveyRatings[2]}
+							onChange={(_, newValue) => {
+								setSurveyRatings([surveyRatings[0], surveyRatings[1], newValue ?? 0]);
+							}}
+						/>
+					</div>
+				}
+				onClose={() => setShowAlert(false)}
+			/>
 		</div>
 	);
 }
--- model/init.py
+++ model/init.py
@@ -3,13 +3,54 @@ import math
 import numpy as np
 
 class Predictor:
-    def __init__(self):
+    def __init__(self, promedio_calidad_locales: float, promedio_popularidad_locales: float):
         # It will come from the ETL container using shared volumes
         self.data_dir = 'data.csv'
         self.data = pd.read_csv(self.data_dir)
+        self.promedio_calidad_locales = promedio_calidad_locales
+        self.promedio_popularidad_locales = promedio_popularidad_locales
 
-    def balance_impact(self, user_preference: float, price: float, quality: float, pm: float):
+    def predict_score(self, popularidad_local: float, calidad_local: float, productosDict: dict):
         '''
-        The params are:
-        @user_preference: float - The user preference for the balance of price & quality
+        Esta funcion se encarga de predecir un score para un local. En el proceso final de 
+        recomendacion, cada local tendra un score calculado por esta funcion, permitiendo 
+        asi seleccionar el local con mejor score para realizar la compra.
+
+        @param popularidad_local: float - Popularidad del local donde realizar la prediccion
+        @param calidad_local: float - Calidad del local donde realizar la prediccion
+        @param productos: dict - Diccionario con los productos a comprar. Cada producto tiene un ID
+
+        @return retorna el score asi como la lista de productos disponibles en el local
         '''
+
+        # obteniendo los productos que hacen match con el nombre y tamaño de los productos a comprar
+        productos: pd.DataFrame = pd.DataFrame(columns=self.data.columns)
+
+        for producto in productosDict:
+            producto = self.data.loc[(self.data['name'] == producto['name']) & (self.data['unit'] == producto['unit'])]
+            productos = productos.append(producto)
+
+
+        # el dataframe tendra una columna llamada "popularidad" que indica la popularidad de cada producto
+        # esta columna sera utilizada como promedio de popularidad de la canasta a comprar
+        popularidad_productos: float = productos['popularity'].mean()
+        valor_canasta: float = productos['price'].sum()
+        
+        # calculando que tanto se despega la calidad del local de la calidad promedio de los locales,
+        # amortiguando el efecto de la calidad del local en el score final
+        calidad_desviacion: float = (calidad_local - self.promedio_calidad_locales)
+
+        # calculando que tanto se despega la popularidad del local de la popularidad promedio de los locales,
+        # amortiguando el efecto de la popularidad del local en el score final
+        popularidad_desviacion: float = (popularidad_local - self.promedio_popularidad_locales)
+
+        # penalizando al local en proporcion con la cantidad de productos que no tiene. El score se 
+        # calcula tomando en cuenta los [productos] y [productosDict] como conjuntos, por lo que
+        # el score se penaliza en proporcion a la cantidad de productos que no tiene el local
+        score_penalizacion: float = 1 - (len(productos) / len(productosDict))
+
+
+        # calculando el score final
+        score: float = (popularidad_productos * 0.5) + (valor_canasta * 0.2) + (calidad_desviacion * 0.1) + (popularidad_desviacion * 0.1) + (score_penalizacion * 0.1)
+
+        return score, productos
