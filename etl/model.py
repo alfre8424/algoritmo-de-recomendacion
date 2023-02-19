04dc98891d489dcaed681ce66713680e6143477d
Joel Garcia <joel@ecx-labs.com>
2023-02-18 23:55:16 -0500

    bug solved on the predictor

--- etl/model.py
+++ etl/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class PredictorModel:
     '''
     Modelo de rating de locales para determinar el que tiene la mejor canasta
@@ -15,19 +16,22 @@ class PredictorModel:
         bk: float,
         pp: float,
         pl: float,
-        m: list
+        m: int,
+        n: int
     ):
         '''
         k: np.array de ponderacion por pregunta
         q: np.array matriz de calidad obtenido del sistema
         eta: int valor maximo con que un usuario puede calificar la calidad
-        beta: int valor minimo con que un usuario puede calificar la popularidad
+        beta: int valor minimo con que un usuario puede calificar la
+            popularidad
         s: np.array reivews de popularidad recibida en el sistema
         sk: float impacto de la popularidad recibida en el sistema
         bk: flaot impacto de la popularidad recibida por encuestas
         pp: precio promedio de la canasta en otros locales
         pl: precio promedio de la canasta en el local
-        m: list de productos no disponibles en el local que si estan en la canasta
+        m: int numero de productos no disponibles en el local
+        n: int cantidad de productos disponibles en el local
         '''
         self.k = k
         self.q = q
@@ -39,6 +43,7 @@ class PredictorModel:
         self.pp = pp
         self.pl = pl
         self.m = m
+        self.n = n
 
     def __quality(self) -> float:
         '''
@@ -67,14 +72,23 @@ class PredictorModel:
 
         return alpha/self.s.max()
 
-    def score(self)->float:
+    def score(self) -> float:
         '''
         Calcula el score final del local
         '''
         a = self.__popularidad() * self.__quality()
-        b = self.pl/(self.pp + 1e-16) if self.pl != 0 and self.pp != 0 else 1e10
-        dr = len(self.m)
 
-        f = a/((b*(dr + 1) + 1e-16))
+        # entre mas alta sean las canastas en otros locales entonces mayor sera
+        # el score para el local actual
+        b = (self.pp/(1 + self.pl)) if (self.pl != 0 and self.pp != 0)\
+            else 1
+
+        f = a*b*(1 + self.n) / (1 + self.m + self.n)
+
+        print(f"Popularidad local: {self.__popularidad()}")
+        print(f"Calidad local: {self.__quality()}")
+        print("Precio canasta en local: ", self.pl)
+        print("Precio canasta en otros locales: ", self.pp)
+        print("Productos faltantes: ", self.m)
 
         return f
--- etl/server.py
+++ etl/server.py
@@ -7,21 +7,22 @@ from preprocessor import Commerce, Preprocessor
 
 app = Flask(__name__)
 
+
 # definiendo rutas
 @app.route('/test')
 def test():
     return 'Hola Mundo'
 
+
 @app.route('/api/v1/recomendar', methods=['POST'])
 def recomendar():
-    if 'canasta' in request.json:
+    if 'canasta' in list(request.json if request.json else []):
         # decodificando json
-        canasta = request.json['canasta']
-        # TODO: remove it
-        available = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+        canasta = list(request.json['canasta'] if request.json else [])
         processor = Preprocessor()
         gz: Commerce = processor.gonzalozambrano
-        gz_data: pd.DataFrame = gz.data
+        gz_data: pd.DataFrame = gz.data if gz.data is not None \
+            else pd.DataFrame()
 
         productos_disponibles = []
         productos_no_disponibles = []
@@ -39,16 +40,17 @@ def recomendar():
         precio_global_gz = sum([x['price'] for x in productos_disponibles])
 
         model = PredictorModel(
-            k = np.array([0.2, 0.7, 0.1]),
-            q = np.array([[4, 3, 4], [5, 3, 5], [1, 4, 3]]),
-            eta = 5,
-            beta = 3,
-            s = np.array([3, 3, 2, 1, 3, 2, 2, 3, 3, 1, 2, 3]),
-            sk = 0.8,
-            bk = 0.2,
-            pp = precio_global_gz, # TODO: change it
-            pl = precio_global_gz,
-            m = [x for x in canasta if x not in available]
+            k=np.array([0.2, 0.7, 0.1]),
+            q=np.array([[5, 5, 5], [5, 5, 5], [5, 5, 5]]),
+            eta=5,
+            beta=3,
+            s=np.array([3] * 10),
+            sk=0.8,
+            bk=0.2,
+            pp=precio_global_gz,  # TODO: change it
+            pl=precio_global_gz,
+            m=len(productos_no_disponibles),
+            n=len(productos_disponibles)
         )
 
         score = model.score()
@@ -58,11 +60,13 @@ def recomendar():
             'mejor_local': 'Gonzalo Zambrano',
             'score': score,
             'precio': precio_global_gz,
-            'mensaje': f'Hay {len(productos_no_disponibles)} productos no disponibles en el local',
+            'mensaje': f'Hay {len(productos_no_disponibles)} productos no \
+                disponibles en el local',
             'productos_faltantes': productos_no_disponibles
         })
 
     return 'no se ha recibido la canasta'
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8300, host='0.0.0.0')
